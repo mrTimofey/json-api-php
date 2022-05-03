641e68ad790e0d9105847ab793173670d14b9e11
Timofey <timoxa17@gmail.com>
2022-05-03 15:12:59 +0300

    fix deprecation

--- src/Document.php
+++ src/Document.php
@@ -226,7 +226,7 @@ class Document implements JsonSerializable
      *
      * @return array
      */
-    public function jsonSerialize()
+    public function jsonSerialize(): array
     {
         return $this->toArray();
     }
